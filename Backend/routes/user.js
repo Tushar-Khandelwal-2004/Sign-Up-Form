1ec99742667ce0d487c01aec7059798b33769479
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-01-02 22:31:05 +0530

    Finalised the Backend and Initialised the frontend (react) for it.

---
+++ Backend/routes/user.js
@@ -0,0 +1,56 @@
+const { Router } = require("express");
+const { userModel } = require("../db");
+const { z } = require("zod");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userRouter = Router();
+const JWT_SECRET = process.env.JWT_USER;
+
+userRouter.post("/signup", async function (req, res) {
+    const requiredBody = z.object({
+        email: z.string().email(),
+        password: z.string().max(100).min(4),
+        firstname: z.string().min(1).max(200),
+        lastname: z.string().min(1).max(200)
+    })
+    const parsedBody = requiredBody.safeParse(req.body);
+    if (!parsedBody.success) {
+        return res.json({
+            success: false,
+            error: parsedBody.error.errors
+        })
+    }
+
+    const { email, password, firstname, lastname } = req.body;
+
+    try {
+        const existingUser = await userModel.findOne( { email } );
+        if (existingUser) {
+            return res.json({
+                success: false,
+                message: "User already exists."
+            })
+        }
+        const hashedPassowrd = await bcrypt.hash(password, 5);
+        await userModel.create({
+            email: email,
+            password: hashedPassowrd,
+            firstname: firstname,
+            lastname: lastname
+        })
+        res.json({
+            success: true,
+            message: "You have successfully signed up!"
+        })
+    } catch (e) {
+        return res.json({
+            success: false,
+            message: "An error occurred while signing up"
+        });
+    }
+
+})
+module.exports={
+    userRouter:userRouter
+}
